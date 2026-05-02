2ef5deb5c564786e34a1362798f3c3d86442cb07
Jehadkawash <jehad234kawash@yahoo.com>
2026-05-02 03:36:56 +0300

    First version of Taekwondo App

---
+++ firebase-config.js
@@ -0,0 +1,21 @@
+// استدعاء مكاتب فايربيس للإصدار 10
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
+
+// إعدادات مشروعك اللي نسختها
+const firebaseConfig = {
+  apiKey: "AIzaSyC52D9vAQcVtmq4du9F8M2iDbmmzKI2Bpk",
+  authDomain: "tkd-registration.firebaseapp.com",
+  projectId: "tkd-registration",
+  storageBucket: "tkd-registration.firebasestorage.app",
+  messagingSenderId: "396167495302",
+  appId: "1:396167495302:web:af62fc1b5029f5e0ff5b07"
+};
+
+// تشغيل فايربيس
+const app = initializeApp(firebaseConfig);
+
+// تصدير قاعدة البيانات ونظام الدخول عشان نستخدمهم في باقي الصفحات
+export const db = getFirestore(app);
+export const auth = getAuth(app);
