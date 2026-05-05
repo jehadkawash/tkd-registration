6cb26cfdf86072127ff8d562a2fde5e368916165
Jehadkawash <jehad234kawash@yahoo.com>
2026-05-05 17:34:56 +0300

    Add login page and protect routes

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,3 @@
-// firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
